eac402e5f3782908466aeb387400a32fc7b2549b
shradhaN <shradha.neupane@yahoo.com>
2018-10-26 13:33:00 +0545

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,18 @@ User.create!(name: "Example User",
                 activated_at: Time.zone.now )
 end
 
+# Micrposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each{ |user| user.microposts.create!(content: content) }
 end
+
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[5..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -86,5 +86,38 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  # test follow unfollow
+  test 'should follow and unfollow a user' do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert  archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test 'feed should have the right posts' do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+
+    # posts from the followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # post from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 
 end
