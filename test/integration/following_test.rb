fdfe8e50273a0665001b88bbc1df016307320293
shradhaN <shradha.neupane@yahoo.com>
2018-10-26 13:41:19 +0545

    Add test for feed in homepage

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -52,6 +52,12 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_difference '@user.following.count', -1 do
       delete relationship_path(relationship), xhr: true
     end
+  end
 
+  test 'feed on Home page' do
+    get root_path
+    @user.feed.paginate(page:1).each do |micropost|
+      assert_match CGI.escapeHTML(@user.microposts.first.content), response.body
+    end
   end
 end
